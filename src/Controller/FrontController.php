4d3f34a8a272e1be06cf44fb8a760317be00057e
Quentin Ramecourt <pro.quentin.ramecourt@gmail.com>
2022-07-26 14:14:22 +0200

    ajout d'un controleuer Admin via make:crud

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App_Repository\ArticleRepository;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     #[Route('/', name: 'app_front_index')]
-    public function index(): Response
+    public function index(ArticleRepository $repository): Response
     {
         $articles = $repository->findNlast(5);
 
